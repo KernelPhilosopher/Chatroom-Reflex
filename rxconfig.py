5eeb911281d46e492f1d8a1a3a36d1378d79b4a6
KernelPhilosopher <kernelphilosopher@gmail.com>
2025-08-18 22:07:06 -0500

    Update configuration for production

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Chatroom_Reflex",
+    api_url="https://chatroom-reflex.onrender.com",
+    deploy_url="https://chatroom-reflex.onrender.com",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
